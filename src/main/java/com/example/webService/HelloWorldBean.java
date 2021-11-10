16909443984928e139dd8e010ee02d6aaf868508
taseon <ats3059@naver.com>
2021-11-10 21:49:28 +0900

    first commit

---
+++ src/main/java/com/example/webService/HelloWorldBean.java
@@ -0,0 +1,13 @@
+package com.example.webService;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+    private String message;
+
+
+
+}
