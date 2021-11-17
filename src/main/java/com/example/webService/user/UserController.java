d9d2a717c4c38977481b6cefa0f8873bc1f35dba
ats3059 <ats3059@naver.com>
2021-11-17 23:33:04 +0900

    delete method study commit

--- src/main/java/com/example/webService/user/UserController.java
+++ src/main/java/com/example/webService/user/UserController.java
@@ -37,6 +37,14 @@ public class UserController {
                 .build();
     }
 
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User user = service.deleteById(id);
+        if(user == null){
+            throw  new UserNotFoundException(String.format("ID[%s] not found" , id));
+        }
+    }
+
 
 
 
